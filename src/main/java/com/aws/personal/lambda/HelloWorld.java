557be4cb3f61bee42949f040157ec7a589340978
ramizgit <rramiz.rraza@gmail.com>
2023-03-12 20:48:33 +0530

    Adding lambda program and moving s3 related classes to s3 package

---
+++ src/main/java/com/aws/personal/lambda/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.aws.personal.lambda;
+
+public class HelloWorld {
+
+    public String message()
+    {
+        return "Hello World! This is my first aws lambda java program sourced from S3. I am very much exicted about it! adding some extra text";
+    }
+}
